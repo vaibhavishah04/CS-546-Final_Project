981d4149240aaa7507c72a18185eaa9d8455c367
RegalTurtle <thysvanderschoot@gmail.com>
2024-12-13 17:20:05 -0500

    Removed unused middleware from route /reporting and made router return when it renders

--- CS_546_NJFloodNet/routes/reporting.js
+++ CS_546_NJFloodNet/routes/reporting.js
@@ -1,24 +1,13 @@
-import {Router} from "express";
+import { Router } from "express";
 import measurementdata from "../data/measurements.js";
 import dashboardrouter from "./dashboard.js";
 const router = Router();
 
-router.route("/").get((req, res, next)=>{
-
-    next();
-},
-
-async (req, res) =>
-{
-    try{
-        res.render("pages/reporting");
-    }
-    catch(e)
-    {
-        res.status(500).send({error :'Internal Server Error'})
-    }
-
-}
-
-);
+router.route("/").get(async (req, res) => {
+  try {
+    return res.render("pages/reporting");
+  } catch (e) {
+    return res.status(500).send({ error: "Internal Server Error" });
+  }
+});
 export default router;
