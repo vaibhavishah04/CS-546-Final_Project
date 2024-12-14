21933d72e54181beb7e53b41dfea06a3915fa0c4
RegalTurtle <thysvanderschoot@gmail.com>
2024-12-13 19:09:15 -0500

    Cleaning up app.js

--- CS_546_NJFloodNet/app.js
+++ CS_546_NJFloodNet/app.js
@@ -1,32 +1,10 @@
-/**
- * Initialize the Express application.
- * Configure middlewares (body-parser, cors).
- * Connect routes from the `routes/` folder.
- * Add error handling and a 404 fallback route.
- */
-
-// Initialize and configure the Express application
-// function initializeExpressApp() {}
-
-// Add middleware configurations
-// function configureMiddlewares(app) {}
-
-// Connect API routes to the application
-// function connectRoutes(app) {}
-
-// Handle 404 errors
-// function handle404(app) {}
-
-// Global error handler
-// function handleErrors(app) {}
-
-const port = 3000;
-
 import express from "express";
-const app = express();
 import configRoutes from "./routes/index.js";
 import session from "express-session";
+const port = 3000;
+const app = express();
 
+// Middleware definitions
 const rewriteUnsupportedBrowserMethods = (req, res, next) => {
   if (req.body && req.body._method) {
     req.method = req.body._method;
@@ -43,7 +21,6 @@ const setUserInLocals = (req, res, next) => {
   next();
 };
 
-// TODO: maybe move this into another file?
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(rewriteUnsupportedBrowserMethods);
@@ -70,4 +47,3 @@ app.listen(port, () => {
   console.log("We've now got a server!");
   console.log(`Your routes will be running on http://localhost:${port}`);
 });
-
